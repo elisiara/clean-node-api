392c964e5325de229dc3ca742e4f39634d1e4b44
elisiara <elisiara@xc-erp.com>
2020-09-13 00:53:40 -0300

    feat: ensure SignUpController returns 400 if no password Confirmation is provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -4,7 +4,7 @@ import { HttpResponse, HttpRequest } from './../protocols/http'
 
 export class SignUpController {
   handle (httpRequest: HttpRequest): HttpResponse {
-    const requiredFiels = ['name', 'email', 'password']
+    const requiredFiels = ['name', 'email', 'password', 'passwordConfirmation']
     for (const field of requiredFiels) {
       if (!httpRequest.body[field]) {
         return badRequest(new MissingParamError(field))
