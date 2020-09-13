99387abf4aed94a9030c3a5fbc8220529b906b70
elisiara <elisiara@xc-erp.com>
2020-09-13 00:46:01 -0300

    refactor(signup): simplify code

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -4,12 +4,11 @@ import { HttpResponse, HttpRequest } from './../protocols/http'
 
 export class SignUpController {
   handle (httpRequest: HttpRequest): HttpResponse {
-    if (!httpRequest.body.name) {
-      return badRequest(new MissingParamError('name'))
-    }
-
-    if (!httpRequest.body.email) {
-      return badRequest(new MissingParamError('email'))
+    const requiredFiels = ['name', 'email']
+    for (const field of requiredFiels) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamError(field))
+      }
     }
   }
 }
