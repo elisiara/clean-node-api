6eba901256c606d6fd26e2d4022cc71152e69d0a
elisiara <elisiara@xc-erp.com>
2020-09-13 01:07:12 -0300

    refactor: make SignUpCOntroller conforms to a protocol

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,8 +1,9 @@
+import { Controller } from './../protocols/controller'
 import { badRequest } from './../helpers/http-helper'
 import { MissingParamError } from './../errors/missing-param-error'
 import { HttpResponse, HttpRequest } from './../protocols/http'
 
-export class SignUpController {
+export class SignUpController implements Controller {
   handle (httpRequest: HttpRequest): HttpResponse {
     const requiredFiels = ['name', 'email', 'password', 'passwordConfirmation']
     for (const field of requiredFiels) {
